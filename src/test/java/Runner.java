59fa3d3416dfd7b57c10c3a08f565ef698551837
abdullah-aydogan <abdullahaydogan245@outlook.com>
2024-02-27 10:30:57 +0300

    Add 3 note adding codes

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -7,19 +7,22 @@ public class Runner extends BaseTest {
     @Test
     public void startTest() {
 
-        // Not Ekleme Testi
-        // ------------------------------------------------------------------
-        methods.checkPage(Elements.recyclerView, "Notlar listesi");
-        methods.clickElement(Elements.addNoteButton);
-        methods.checkPage(Elements.addNoteText, "Not ekleme");
-        methods.clickElement(Elements.titleEditText);
-        methods.enterText(Elements.titleEditText, "Not Başlığı");
-        methods.clickElement(Elements.contentEditText);
-        methods.enterText(Elements.contentEditText, "Not İçeriği");
-        methods.clickElement(Elements.saveNoteButton);
-        methods.checkPage(Elements.recyclerView, "Notlar listesi");
-        methods.checkDateTime(Elements.dateTime);
-        // -----------------------------------------------------------------
+        for(int i=0; i<3; i++) {
+
+            // Not Ekleme Testi
+            // ------------------------------------------------------------------
+            methods.checkPage(Elements.recyclerView, "Notlar listesi");
+            methods.clickElement(Elements.addNoteButton);
+            methods.checkPage(Elements.addNoteText, "Not ekleme");
+            methods.clickElement(Elements.titleEditText);
+            methods.enterText(Elements.titleEditText, (i + 1) + ". Not Başlığı");
+            methods.clickElement(Elements.contentEditText);
+            methods.enterText(Elements.contentEditText, (i + 1) + ". Not İçeriği");
+            methods.clickElement(Elements.saveNoteButton);
+            methods.checkPage(Elements.recyclerView, "Notlar listesi");
+            methods.checkDateTime(Elements.dateTime);
+            // -----------------------------------------------------------------
+        }
 
         // Not Düzenleme Testi
         // --------------------------------------------------------------------------
